65207b2cc070e222c0dfba8e136632161aa2f296
Parth Ahuja <parthahuja2003@gmail.com>
2024-07-07 21:31:59 +0530

    modification

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // src/App.jsx
 import { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
