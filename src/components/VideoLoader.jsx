96270c5946090e4a5a290cc527912557a9b290f1
Parth Ahuja <parthahuja2003@gmail.com>
2024-08-25 15:38:50 +0530

    updated hero

---
+++ src/components/VideoLoader.jsx
@@ -0,0 +1,11 @@
+import "../styles/videoLoader.css"
+const VideoLoader = () => {
+  return (
+    <div className="loaderVideo">
+      <div className="inner one"></div>
+      <div className="inner two"></div>
+      <div className="inner three"></div>
+    </div>
+  );
+}
+export default VideoLoader
