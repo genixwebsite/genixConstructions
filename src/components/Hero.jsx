6a2c4d1e00a9568c4e4fdd2104fb2ed0894bd1f9
Parth Ahuja <parthahuja2003@gmail.com>
2024-08-29 23:29:19 +0530

    updated hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -45,31 +45,41 @@ const Hero = () => {
 
   return (
     <div className="relative flex flex-col items-center justify-center w-full h-screen">
-      <div
-        className={`absolute top-0 left-0 w-full h-full bg-cover bg-center transition-opacity duration-500 ${videoLoaded ? "opacity-0" : "opacity-100"}`}
-        style={{ backgroundImage: "url('')" }}
-      ></div>
       <video
         id="hero-video"
-        className={`absolute top-0 left-0 w-full h-full object-cover transition-opacity duration-500 ${videoLoaded ? "opacity-100" : "opacity-0"}`}
+        className="absolute top-0 left-0 w-full h-full object-cover"
         src="/video.mp4"
         autoPlay
         loop
         muted
+        preload="auto"
+        poster="/videoPoster.png"
       />
+
       <div className="absolute top-0 left-0 w-full h-full bg-black opacity-60"></div>
       <div className="relative text-center z-10 text-white flex flex-col items-center justify-center h-full">
-        <h1 className="text-6xl font-light mb-4">Genix Constructions</h1>
-        <h2 className="text-xl p-2 rounded-xl bg-[#CFA35D] font-medium text-white">
-          You dream it, We build it!
-        </h2>
+        <div className="text-center">
+          <h1 className="text-6xl font-light">Genix Constructions</h1>
+          <h2 className="text-2xl font-extralight text-gray-100">
+            <span className="bg-[#CFA35D] px-1">
+              {" "}
+              You dream it, We build it!{" "}
+            </span>
+          </h2>
+        </div>
         <div className="absolute bottom-16 text-center">
           {showLine && (
             <div className="w-2 h-24 bg-[#CFA35D] mb-4 animate-line"></div>
           )}
         </div>
-        <div className={`absolute bottom-16 transition-opacity duration-500 ${!showLine ? 'opacity-100 delay-500' : 'opacity-0'}`}>
-          <h2 className="text-xl font-semibold p-2 w-96 h-24 text-white">{text}</h2>
+        <div
+          className={`absolute bottom-16 transition-opacity duration-500 ${
+            !showLine ? "opacity-100 delay-500" : "opacity-0"
+          }`}
+        >
+          <h2 className="text-xl font-semibold p-2 w-96 h-24 text-white">
+            {text}
+          </h2>
         </div>
       </div>
       <style jsx>{`
@@ -87,7 +97,7 @@ const Hero = () => {
             opacity: 1;
           }
           100% {
-            transform: translate(-100px, 0) rotate(360deg);
+            transform: translate(-150px, 0) rotate(360deg);
             opacity: 0;
           }
         }
